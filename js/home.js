d9b3fcc53cbb8162eeb3b17867ae63391c997aa4
LEANDRO DOS REIS FILHO <25203700@dsn127m1246734.cfp127aluno.local>
2026-07-27 15:13:23 -0300

    adicionando imagens

--- js/home.js
+++ js/home.js
@@ -6,5 +6,10 @@ export async function criarHome(){
     texto.className = 'textoInicial'
     texto.textContent = 'Escolha um curso para gerenciar'
 
+    const image = document.createElement('img')
+    image.className = 'aparelhos'
+    image.src = "../img/devices.png"
+    document.main.appendChild(image)
+
     return texto
 }
